8f9079e452e6b4e27d12bafd61629f4644995586
sisyphi <jordan.m.sibug@gmail.com>
2024-05-22 15:52:44 +0800

    feat(wip): Adds small updates to Hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1 class="font-poppins">Welcome to SvelteKit</h1>
-<p class="font-generalsans">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="bg-offwhite">
+    <section id="hero" class="">
+        <div class="">
+            <h1 class="text-xl font-bold font-poppins text-brown">Your favorite NGO that helps out cats and people who help cats out</h1>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </section>
+</section>
