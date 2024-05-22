b4719910c4820afd44f8974d7e25001a59025fd0
sisyphi <jordan.m.sibug@gmail.com>
2024-05-22 15:33:28 +0800

    feat: Adds basic header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<header class="bg-brown">
+    <div class="flex justify-between max-w-4xl mx-auto text-xl font-bold text-center align-middle font-poppins text-offwhite">
+        <div id="logo" class="flex justify-center p-8 align-middle">
+            Pretzel's Place
+        </div>
+        <div id="nav">
+            <nav>
+                <ul class="flex flex-row gap-8 p-8">
+                    <a href="/" class=""><li>Home</li></a>
+                    <a href="/about-us"><li>About Us</li></a>
+                    <a href="/our-cats"><li>Our Cats</li></a>
+                    <a href="/donation"><li class="text-orange">Help Out!</li></a>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
