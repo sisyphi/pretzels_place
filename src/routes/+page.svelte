6a95a806ec35455f75112c00c657ff56cb90a690
sisyphi <jordan.m.sibug@gmail.com>
2024-05-23 14:55:07 +0800

    page(home): Adds Hero & Stats sections
    Finishes the Hero section just w/o hero image
    
    Finishes the Stats section
    
    Changes the default Tailwind screen breakpoints to add xs: 525px

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,49 @@
 <section class="bg-offwhite">
-    <section id="hero" class="">
-        <div class="">
-            <h1 class="text-xl font-bold font-poppins text-brown">Your favorite NGO that helps out cats and people who help cats out</h1>
+    <section id="hero" class="p-8 md:py-16 md:grid md:max-w-4xl md:grid-cols-5 md:grid-rows-1 md:mx-auto">
+        <div class="relative z-10 content-center md:row-end-1 md:col-start-1 md:col-end-4 md:row-start-1 md:max-w-lg">
+            <h1 class="text-3xl font-bold sm:text-5xl md:text-6xl font-poppins text-brown">Your favorite NGO that helps out cats and people who help cats out</h1>
         </div>
-        <div>
-            <img src="" alt="">
+        <div class="relative z-0 py-16 md:max-w-lg md:col-start-3 md:col-end-6 md:row-start-1 md:row-end-1 bg-orange">
+            <!-- <img src="" alt=""> -->
+            <!-- <h1 class="text-6xl font-bold font-poppins text-brown">Your favorite NGO that helps out cats and people who help cats out</h1> -->
+        </div>
+    </section>
+
+    <section id="stats" class="bg-brown">
+        <div class="grid gap-4 py-4 xs:py-0 xs:max-w-4xl xs:mx-auto xs:grid-cols-3 xs:grid-rows-1">
+            <div class="xs:col-start-1 xs:col-end-2">
+                <div class="xs:grid xs:grid-cols-1 xs:grid-rows-3">
+                    <div class="grid xs:pt-8 xs:grid-rows-1 xs:row-start-1 xs:row-end-3">
+                        <span class="self-center w-24 h-24 col-start-1 col-end-1 row-start-1 row-end-1 rounded-full justify-self-center bg-orange"></span>
+                        <span class="self-center col-start-1 col-end-1 row-start-1 row-end-1 justify-self-center"><p class="text-6xl font-bold text-center text-offwhite font-poppins">200+</p></span>
+                    </div>
+                    <div class="xs:row-start-3 xs:row-end-4">
+                        <p class="text-2xl font-bold text-center text-offwhite font-poppins md:text-4xl">cats fed</p>
+                    </div>
+                </div>
+            </div>
+            <div class="xs:col-start-2 xs:col-end-3">
+                <div class="xs:grid xs:grid-cols-1 xs:grid-rows-3">
+                    <div class="grid xs:pt-8 xs:grid-rows-1 xs:row-start-1 xs:row-end-3">
+                        <span class="self-center w-24 h-24 col-start-1 col-end-1 row-start-1 row-end-1 rounded-full justify-self-center bg-orange"></span>
+                        <span class="self-center col-start-1 col-end-1 row-start-1 row-end-1 justify-self-center"><p class="text-6xl font-bold text-center text-offwhite font-poppins">32+</p></span>
+                    </div>
+                    <div class="xs:row-start-3 xs:row-end-4">
+                        <p class="text-2xl font-bold text-center text-offwhite font-poppins md:text-4xl">cats rescued</p>
+                    </div>
+                </div>
+            </div>
+            <div class="xs:col-start-3 xs:col-end-4">
+                <div class="xs:grid xs:grid-cols-1 xs:grid-rows-3">
+                    <div class="grid xs:pt-8 xs:grid-rows-1 xs:row-start-1 xs:row-end-3">
+                        <span class="self-center w-24 h-24 col-start-1 col-end-1 row-start-1 row-end-1 rounded-full justify-self-center bg-orange"></span>
+                        <span class="self-center col-start-1 col-end-1 row-start-1 row-end-1 justify-self-center"><p class="text-6xl font-bold text-center text-offwhite font-poppins">23+</p></span>
+                    </div>
+                    <div class="xs:row-start-3 xs:row-end-4">
+                        <p class="text-2xl font-bold text-center text-offwhite font-poppins md:text-4xl">cats in care</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </section>
