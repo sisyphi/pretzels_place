41edb80f5610109cfcce89c83242cf58b125efe9
sisyphi <jordan.m.sibug@gmail.com>
2024-05-24 15:44:14 +0800

    init(bits-ui): Inits bits-ui

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Accordion } from 'bits-ui';
+</script>
+
+<Accordion.Root>
+	<Accordion.Item value="1">
+		<Accordion.Header>
+			<Accordion.Trigger>How can I adopt a cat from your organization?</Accordion.Trigger>
+		</Accordion.Header>
+		<Accordion.Content>First accordion content</Accordion.Content>
+	</Accordion.Item>
+	<Accordion.Item value="2">
+		<Accordion.Header>
+			<Accordion.Trigger>Are there requirements to become a foster parent?</Accordion.Trigger>
+		</Accordion.Header>
+		<Accordion.Content>Second accordion content</Accordion.Content>
+	</Accordion.Item>
+	<Accordion.Item value="3">
+		<Accordion.Header>
+			<Accordion.Trigger>Third</Accordion.Trigger>
+		</Accordion.Header>
+		<Accordion.Content>Third accordion content</Accordion.Content>
+	</Accordion.Item>
+</Accordion.Root>
