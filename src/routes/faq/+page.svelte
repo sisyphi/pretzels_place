fa5feb82da1a05b994316895c4aebb95adefec01
sisyphi <jordan.m.sibug@gmail.com>
2024-05-24 17:20:42 +0800

    page(faq): Adds Accordion to FAQ page
    Properly uses bits-ui Accordion component
    
    Adds CaretDown icon

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,24 +1,56 @@
 <script lang="ts">
 	import { Accordion } from 'bits-ui';
+	import CaretDown from '$lib/icons/CaretDown.svelte';
 </script>
 
-<Accordion.Root>
-	<Accordion.Item value="1">
-		<Accordion.Header>
-			<Accordion.Trigger>How can I adopt a cat from your organization?</Accordion.Trigger>
-		</Accordion.Header>
-		<Accordion.Content>First accordion content</Accordion.Content>
-	</Accordion.Item>
-	<Accordion.Item value="2">
-		<Accordion.Header>
-			<Accordion.Trigger>Are there requirements to become a foster parent?</Accordion.Trigger>
-		</Accordion.Header>
-		<Accordion.Content>Second accordion content</Accordion.Content>
-	</Accordion.Item>
-	<Accordion.Item value="3">
-		<Accordion.Header>
-			<Accordion.Trigger>Third</Accordion.Trigger>
-		</Accordion.Header>
-		<Accordion.Content>Third accordion content</Accordion.Content>
-	</Accordion.Item>
-</Accordion.Root>
+<section id="faq" class="">
+	<div class="xs:max-w-2xl xs:pt-16 mx-auto p-8 md:max-w-4xl">
+		<h1 class="font-poppins text-brown text-4xl font-bold md:text-5xl">Frequently Asked Questions</h1>
+		<Accordion.Root class="py-8">
+			<Accordion.Item value="1" class="border-brown border-b-2 py-4">
+				<Accordion.Header>
+					<Accordion.Trigger class="flex w-full justify-between [&[data-state=open]>svg]:rotate-180">
+						<p class="font-poppins text-brown text-left text-2xl font-bold">How can I adopt a cat from your organization?</p>
+						<CaretDown class="text-brown xs:size-8 size-12 " />
+					</Accordion.Trigger>
+				</Accordion.Header>
+				<Accordion.Content class="font-generalsans text-brown pt-4 font-medium"
+					>If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and
+					social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.</Accordion.Content>
+			</Accordion.Item>
+			<Accordion.Item value="2" class="border-brown border-b-2 py-4">
+				<Accordion.Header>
+					<Accordion.Trigger class="flex w-full justify-between [&[data-state=open]>svg]:rotate-180">
+						<p class="font-poppins text-brown text-left text-2xl font-bold">How can I adopt a cat from your organization?</p>
+						<CaretDown class="text-brown xs:size-8 size-12 " />
+					</Accordion.Trigger>
+				</Accordion.Header>
+				<Accordion.Content class="font-generalsans text-brown pt-4 font-medium"
+					>If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and
+					social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.</Accordion.Content>
+			</Accordion.Item>
+			<Accordion.Item value="3" class="border-brown border-b-2 py-4">
+				<Accordion.Header>
+					<Accordion.Trigger class="flex w-full justify-between [&[data-state=open]>svg]:rotate-180">
+						<p class="font-poppins text-brown text-left text-2xl font-bold">How can I adopt a cat from your organization?</p>
+						<CaretDown class="text-brown xs:size-8 size-12 " />
+					</Accordion.Trigger>
+				</Accordion.Header>
+				<Accordion.Content class="font-generalsans text-brown pt-4 font-medium"
+					>If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and
+					social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.</Accordion.Content>
+			</Accordion.Item>
+			<Accordion.Item value="4" class="border-brown border-b-2 py-4">
+				<Accordion.Header>
+					<Accordion.Trigger class="flex w-full justify-between [&[data-state=open]>svg]:rotate-180">
+						<p class="font-poppins text-brown text-left text-2xl font-bold">How can I adopt a cat from your organization?</p>
+						<CaretDown class="text-brown xs:size-8 size-12 " />
+					</Accordion.Trigger>
+				</Accordion.Header>
+				<Accordion.Content class="font-generalsans text-brown pt-4 font-medium"
+					>If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and
+					social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.</Accordion.Content>
+			</Accordion.Item>
+		</Accordion.Root>
+	</div>
+</section>
