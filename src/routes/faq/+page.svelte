8571c1b778ec7c35207745643f750b814463b643
sisyphi <jordan.m.sibug@gmail.com>
2024-05-27 19:14:17 +0800

    feat: Adds Sanity data pull for the FAQ page instead of hardcoded data

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -2,48 +2,23 @@
 	import { Accordion } from 'bits-ui';
 	import CaretDown from '$lib/icons/CaretDown.svelte';
 
-	const faq_items = [
-		{
-			title: 'How can I adopt a cat from your organization?',
-			content:
-				'If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.',
-		},
-		{
-			title: 'Are there requirements to become a foster parent?',
-			content:
-				'If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.',
-		},
-		{
-			title: 'Can I adopt more than one cat at a time?',
-			content:
-				'If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.',
-		},
-		{
-			title: "How can I support if I can't adopt or foster?",
-			content:
-				'If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.',
-		},
-		{
-			title: 'What should I do if I find a stray cat?',
-			content:
-				'If you find a stray cat, first check for a collar and identification tags. If the cat appears healthy and friendly, it might belong to someone in the neighborhood. You can create flyers and post on local lost-and-found pet websites and social media. If the cat is in distress or injured, contact us or your local animal control for assistance. We can provide advice and resources to help you determine the best course of action.',
-		},
-	];
+	export let data;
+	let faq_items = data.faq_items;
 </script>
 
 <section id="faq">
-	<div class="xs:max-w-2xl xs:pt-16 mx-auto p-8 md:max-w-4xl">
-		<h1 class="font-poppins text-brown text-3xl font-bold md:text-5xl">Frequently Asked Questions</h1>
+	<div class="mx-auto p-8 xs:max-w-2xl xs:pt-16 md:max-w-4xl">
+		<h1 class="font-poppins text-3xl font-bold text-brown md:text-5xl">Frequently Asked Questions</h1>
 		<Accordion.Root class="py-8">
 			{#each faq_items as item, i}
-				<Accordion.Item value="${i}" class="border-brown border-b-2 py-4">
+				<Accordion.Item value="${i}" class="border-b-2 border-brown py-4">
 					<Accordion.Header>
 						<Accordion.Trigger class="flex w-full justify-between [&[data-state=open]>svg]:rotate-180">
-							<p class="font-poppins text-brown text-left text-2xl font-bold">{item.title}</p>
-							<CaretDown class="text-brown xs:size-8 size-12 " />
+							<p class="text-left font-poppins text-2xl font-bold text-brown">{item.title}</p>
+							<CaretDown class="size-12 text-brown xs:size-8 " />
 						</Accordion.Trigger>
 					</Accordion.Header>
-					<Accordion.Content class="font-generalsans text-brown pt-4 font-medium">{item.content}</Accordion.Content>
+					<Accordion.Content class="pt-4 font-generalsans font-medium text-brown">{item.content}</Accordion.Content>
 				</Accordion.Item>
 			{/each}
 		</Accordion.Root>
