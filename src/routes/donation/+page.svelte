b524416e5e0ba3189eb6ba8bc24d054f37c7adab
sisyphi <jordan.m.sibug@gmail.com>
2024-05-31 13:50:54 +0800

    page: Adds Donation page

---
+++ src/routes/donation/+page.svelte
@@ -0,0 +1,47 @@
+<section id="donation" class="">
+	<div class="p-8 mx-auto xs:max-w-2xl xs:pt-16 md:max-w-4xl">
+		<h1 class="pb-4 text-4xl font-bold font-poppins text-brown md:text-5xl">Donation</h1>
+
+		<section id="in-cash-in-kind" class="">
+			<div class="grid gap-8 mx-auto xs:max-w-3xl xs:grid-cols-9 xs:gap-0">
+				<div class="grid gap-4 p-8 bg-brown xs:col-start-1 xs:col-end-5 xs:row-start-1 xs:row-end-1">
+					<div class="grid xs:grid-rows-3">
+						<div class="place-content-center bg-orange xs:col-start-1 xs:col-end-1 xs:row-start-1 xs:row-end-2">
+							<h2 class="text-4xl font-bold text-center font-poppins text-offwhite md:text-5xl">In Cash</h2>
+						</div>
+						<div class="py-4 xs:col-start-1 xs:col-end-1 xs:row-start-2 xs:row-end-4 xs:flex xs:flex-col xs:justify-between xs:gap-8">
+							<div class="pb-8 xs:pb-0">
+								<p class="font-generalsans text-offwhite">You can make a direct impact by donating to our bank accounts. Please use the following bank details:</p>
+							</div>
+							<div>
+								<p class="font-generalsans text-offwhite">
+									GCash Account No.: <span class="font-bold">0123 456 7890</span> <br /> BPI Account No.: <span class="font-bold">0123 456 7890</span> <br /> BDO Account No.: <span class="font-bold">0123 456 7890</span> <br />
+									Account Name: <span class="font-bold">Jinggay Dinsay</span>
+								</p>
+							</div>
+							<!-- <p class="text-2xl font-bold text-left font-poppins text-offwhite md:text-4xl">Creating safe spaces for cats, so we can guarantee their safety</p> -->
+						</div>
+					</div>
+				</div>
+				<div class="grid gap-4 p-8 bg-brown xs:col-start-6 xs:col-end-10 xs:row-start-1 xs:row-end-1">
+					<div class="xs:grid xs:grid-rows-3">
+						<div class="place-content-center bg-orange xs:col-start-1 xs:col-end-1 xs:row-start-1 xs:row-end-2">
+							<h2 class="text-4xl font-bold text-center font-poppins text-offwhite md:text-5xl">In Kind</h2>
+						</div>
+						<div class="py-4 xs:col-start-1 xs:col-end-1 xs:row-start-2 xs:row-end-4 xs:flex xs:flex-col xs:justify-between xs:gap-8">
+							<div class="pb-8 xs:pb-0">
+								<p class="font-generalsans text-offwhite">You can also donate goods instead. We accept food, toys and resources. Please contact us through these channels so we can arrange this:</p>
+							</div>
+							<div>
+								<p class="font-generalsans text-offwhite">
+									Contact No.: <span class="font-bold">0917 123 1234</span> <br /> Messenger Contact: <span class="font-bold">Jinggay Dinsay</span> <br /> Email Address: <span class="font-bold">email@email.com</span>
+								</p>
+							</div>
+							<!-- <p class="text-2xl font-bold text-left font-poppins text-offwhite md:text-4xl">Creating safe spaces for cats, so we can guarantee their safety</p> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</section>
